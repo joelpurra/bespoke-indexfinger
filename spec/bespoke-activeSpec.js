1d3ea69b6557e8b8103553607aa7b7b291538eec
Joel Purra <code+github@joelpurra.com>
2013-11-11 21:44:23 +0100

    Functional code, with tests

--- spec/bespoke-activeSpec.js
+++ spec/bespoke-activeSpec.js
@@ -21,14 +21,38 @@
 
         beforeEach(createDeck);
 
-        describe("deck.slide", function() {
+        describe("deck.getActiveSlide and deck.getActiveSlideIndex", function() {
 
-            beforeEach(function() {
-                deck.slide(0);
+            it("should handle slide 0", function() {
+                expect(deck.getActiveSlide()).toBe(deck.slides[0]);
+                expect(deck.getActiveSlideIndex()).toBe(0);
             });
 
-            it("should not add a useless foobar class to the slide", function() {
-                expect(deck.slides[0].classList.contains("foobar")).toBe(false);
+            it("should handle slide 0 then next", function() {
+                deck.slide(0);
+                deck.next();
+                expect(deck.getActiveSlide()).toBe(deck.slides[1]);
+                expect(deck.getActiveSlideIndex()).toBe(1);
+            });
+
+            it("should handle slide 5", function() {
+                deck.slide(5);
+                expect(deck.getActiveSlide()).toBe(deck.slides[5]);
+                expect(deck.getActiveSlideIndex()).toBe(5);
+            });
+
+            it("should handle slide 5 then prev", function() {
+                deck.slide(5);
+                deck.prev();
+                expect(deck.getActiveSlide()).toBe(deck.slides[4]);
+                expect(deck.getActiveSlideIndex()).toBe(4);
+            });
+
+            it("should handle slide 0 then prev", function() {
+                deck.slide(0);
+                deck.prev();
+                expect(deck.getActiveSlide()).toBe(deck.slides[0]);
+                expect(deck.getActiveSlideIndex()).toBe(0);
             });
         });
     });
